4d3fca4f8f8e3aa556a63a5f52648dba17706b8a
Dziad573 <krystiansmagacz05@gmail.com>
2024-09-29 15:33:32 +0200

    folders, fonts, globals.css

---
+++ car-service/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './styles/globals.css'
+import './styles/theme.css';
+function App() {
+
+  return (
+    <>
+      <p>dwfffffff  346 213 aa</p>
+    </>
+  )
+}
+
+export default App
