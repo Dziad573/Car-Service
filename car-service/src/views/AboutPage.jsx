2a10dc1af3a26c6dfabed91d3360f73687dec161
Dziad573 <krystiansmagacz05@gmail.com>
2024-10-07 21:26:31 +0200

    views

---
+++ car-service/src/views/AboutPage.jsx
@@ -0,0 +1,10 @@
+
+function AboutPage() {
+    return (
+        <div>
+            <h1>Learn more About Us!</h1>
+        </div>
+    );
+}
+
+export default AboutPage;
---
+++ car-service/src/views/CarsPage.jsx
@@ -0,0 +1,10 @@
+
+function CarsPage() {
+    return (
+        <div>
+            <h1>Welcome to the Cars Page!</h1>
+        </div>
+    );
+}
+
+export default CarsPage;
