cad54f6e78fdd35842e9f3ea2de49ce2bfa03601
Dziad573 <krystiansmagacz05@gmail.com>
2024-10-19 17:36:36 +0200

    timer

--- car-service/src/App.jsx
+++ car-service/src/App.jsx
@@ -1,14 +1,35 @@
-import { useState } from 'react'
-import './styles/globals.css'
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import './styles/globals.css';
 import './styles/theme.css';
 import HomePage from './views/HomePage';
-function App() {
+import CarsPage from './views/CarsPage';
+import AboutPage from './views/AboutPage';
+import ContactPage from './views/ContactPage';
+import TopBar from './components/TopBar/TopBar';
+
+function AppContent() {
+  const location = useLocation();
 
   return (
     <>
-      <HomePage/>
+      {location.pathname !== '/' && <TopBar />}
+      
+      <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/cars" element={<CarsPage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+function App() {
+  return (
+    <Router>
+      <AppContent />
+    </Router>
+  );
+}
+
+export default App;
--- car-service/src/components/Hero/Hero.jsx
+++ car-service/src/components/Hero/Hero.jsx
@@ -1,8 +1,10 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { useEffect, useState } from 'react';
+import { NavLink } from 'react-router-dom';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-import { Navigation, Pagination } from 'swiper/modules';  // Zmiana importu modułów
+import { Navigation, Pagination } from 'swiper/modules';
 import styles from './Hero.module.css';
 import car1 from './Challenger.png';
 import car2 from './Charger.png';
@@ -15,6 +17,7 @@ const cars = [
         priceNew: '$800',
         time: '1 day',
         img: car1,
+        expireDate: new Date('2024-10-07T19:31:40'),
     },
     {
         name: 'Lamborghini Aventador J',
@@ -22,6 +25,7 @@ const cars = [
         priceNew: '$800',
         time: '1 day',
         img: car2,
+        expireDate: new Date('2024-10-12T23:59:59'),
     },
     {
         name: 'Lamborghini Aventador J',
@@ -29,9 +33,55 @@ const cars = [
         priceNew: '$800',
         time: '1 day',
         img: car3,
+        expireDate: new Date('2024-10-15T23:59:59'),
     },
-  // Dodaj kolejne samochody
 ];
+
+function Countdown({ expireDate }) {
+    const calculateTimeLeft = () => {
+        const difference = +new Date(expireDate) - +new Date();
+        let timeLeft = {};
+
+        if (difference > 0) {
+            timeLeft = {
+                days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+                hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+                minutes: Math.floor((difference / 1000 / 60) % 60),
+                seconds: Math.floor((difference / 1000) % 60),
+            };
+        }
+
+        return timeLeft;
+    };
+
+    const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setTimeLeft(calculateTimeLeft());
+        }, 1000);
+
+        return () => clearInterval(timer);
+    }, []);
+
+    return (
+        <div className={styles.countdownContainer}> {/* Klasa kontenerowa */}
+            {timeLeft.days !== undefined ? (
+                <div className={styles.countdown}>
+                    <span>{String(timeLeft.days).padStart(2, '0')}:</span>
+                    <span>{String(timeLeft.hours).padStart(2, '0')}:</span>
+                    <span>{String(timeLeft.minutes).padStart(2, '0')}:</span>
+                    <span>{String(timeLeft.seconds).padStart(2, '0')}</span>
+                </div>
+            ) : (
+                <span>Offer expired</span>
+            )}
+        </div>
+    );
+}
+
+
+
 
 function Hero() {
     return (
@@ -42,37 +92,52 @@ function Hero() {
             </div>
             <div className={styles.SliderComponent}>
             <Swiper
-                modules={[Navigation, Pagination]}  // Moduły zaktualizowane
+                modules={[Navigation, Pagination]}
                 navigation
                 pagination={{ clickable: true }}
                 spaceBetween={50}
                 slidesPerView={1}
                 loop={true}
-                
-                autoplay={{ delay: 10000 }}
+                autoplay={{ delay: 100 }}
             >
                 {cars.map((car, index) => (
                 <SwiperSlide key={index}>
                     <div className={styles.Slide}>
                         <div className={styles.HeroInner}>
-                            <h1>The offer is active for: </h1>
+                            <h1>
+                                The offer is active for:
+                            </h1>
+                                <Countdown expireDate={car.expireDate} className={styles.countdown} />
+                            
+                            
                         </div>
                     <img src={car.img} alt={car.name} className={styles.CarImage} />
                     <div className={styles.CarDetails}>
 
                         <div>
                             <h2>{car.name}</h2>
+                            
                             <div className={styles.Buttons}>
+                                <NavLink 
+                                    to={`/${car.name.replace(/\s+/g, '-')}`}
+                                    end
+                                >
                                 <div className={styles.DetailsButton}>
                                     <svg xmlns="http://www.w3.org/2000/svg" 
                                         width="28" 
                                         height="28" 
                                         viewBox="0 0 24 24">
-                                        <g fill="none" stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0-4 0"/>
-                                        <path d="M21 12q-3.6 6-9 6t-9-6q3.6-6 9-6t9 6"/></g>
+                                        <g fill="none" stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+                                            <path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0-4 0"/>
+                                            <path d="M21 12q-3.6 6-9 6t-9-6q3.6-6 9-6t9 6"/>
+                                        </g>
                                     </svg>
-                                    <div>View Details</div>
+
+                                    
+                                        <div className={styles.details}>View Details</div>
+                                    
                                 </div>
+                                </NavLink>
                             </div>
                         </div>
 
@@ -96,5 +161,6 @@ function Hero() {
         </div>
     );
 }
+
 
 export default Hero;
